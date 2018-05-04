eebe5931ae9329819cc8e52d2881e040278e063b
Gianni Riccardi <riccardi.gianni@gmail.com>
2018-05-04 19:40:02 +0200

    Condition 8 caracteres min pour pwd de signup

--- frontend/login-page.php
+++ frontend/login-page.php
@@ -54,8 +54,8 @@
                     <input type="text" name="username" class="reg-username" tabindex="1" id="reg-username" aria-required="true" required>
                     <label for="reg-email">Email</label>
                     <input type="text" name="email" class="reg-email" autocapitalize="off" tabindex="1" id="reg-email" aria-required="true" autofocus="autofocus" required>
-                    <label for="reg-password">Mot de passe</label>
-                    <input type="password" name="session_password" class="reg-password" id="reg-password" aria-required="true" tabindex="1" autocomplete="new-password" required>
+                    <label for="reg-password">Mot de passe (8 caractères minimum)</label>
+                    <input type="password" name="session_password" class="reg-password" id="reg-password" aria-required="true" tabindex="1" autocomplete="new-password" onkeyup="checkPasswordMatch()" required>
                     <label for="reg-password">Confirmez le mot de passe</label>
                     <input type="password" name="session_password" class="reg-password" id="reg-password-confirm" aria-required="true" tabindex="1" autocomplete="new-password" onkeyup="checkPasswordMatch()" required>
                     <input tabindex="4" id="registration-submit" class="registration submit-button" type="submit" name="signup" value="S'inscrire">
@@ -82,7 +82,7 @@
                 var password = document.getElementById("reg-password");
                 var confirmPassword = document.getElementById("reg-password-confirm");
                 var regpassword = document.getElementsByClassName("reg-password");
-                if (password.value != confirmPassword.value) {
+                if (password.value != confirmPassword.value || password.value.length < 8) {
                     for (i = 0; i < regpassword.length; i++) {
                         regpassword[i].style.borderColor = "#ef0000";
                     }
@@ -98,11 +98,6 @@
                 }
 
             }
-
-            $(document).ready(function () {
-                document.getElementById("reg-password").keyup(checkPasswordMatch);
-                document.getElementById("reg-password-confirm").keyup(checkPasswordMatch);
-            });
         </script>
 
 

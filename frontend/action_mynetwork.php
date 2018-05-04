c1f7b110cea108f98675dfa265b8d2daabd0a1d7
Reinim <minier.theo@gmail.com>
2018-05-04 18:59:53 +0200

    SQL Reseau AMIS

--- frontend/action_mynetwork.php
+++ frontend/action_mynetwork.php
@@ -1,12 +1,35 @@
 <?php
+session_start();
+
 //function mynetwork1(){
+/*SELECT first_name , last_name 
+FROM users
+WHERE username IN (SELECT 
+CASE WHEN username_user1 = '".$_SESSION['myusername']."' THEN username_user2 
+	 WHEN username_user2 = '".$_SESSION['myusername']."' THEN username_user1 
+     ELSE NULL 
+     END AS NewField 
+FROM contacts WHERE username_user2 = '".$_SESSION['myusername']."' OR username_user1 = '".$_SESSION['myusername']."')
+
+ */
+
+ $sql = "SELECT first_name , last_name 
+FROM users
+WHERE username IN (SELECT 
+CASE WHEN username_user1 = '".$_SESSION['myusername']."' THEN username_user2 
+	 WHEN username_user2 = '".$_SESSION['myusername']."' THEN username_user1 
+     ELSE NULL 
+     END AS NewField 
+FROM contacts WHERE username_user2 = '".$_SESSION['myusername']."' OR username_user1 = '".$_SESSION['myusername']."')";
+;
+echo $sql;
     
-$database='linkedoff';
+/*$database='linkedoff';
 $db_handle=mysqli_connect('localhost', 'root', 'root');       
 $db_found=mysqli_select_db($db_handle,$database);
 
 if($db_found) {            
-    $sql = "SELECT username FROM users WHERE username = '".$_POST["uname"]."'"; 
+   // $sql = "SELECT username FROM users WHERE username = '".$_POST["uname"]."'"; 
     $result = mysqli_query($db_handle, $sql) or die(mysql_error());
     
     while($data = mysqli_fetch_assoc($result)) {
@@ -26,6 +49,6 @@ else { echo "Base de données non trouvée."; }
 
 mysqli_close($db_handle);
 //}
-
+*/
 
 ?>

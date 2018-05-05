398f053f10e9dc1d3c6a57165435684b79d77f4f
Joel <joel.vieira@edu.ece.fr>
2018-05-05 17:58:38 +0200

    ajout notifs share

--- frontend/action_share.php
+++ frontend/action_share.php
@@ -6,17 +6,17 @@ session_start();
     $db_found=mysqli_select_db($db_handle,$database);
 
     if($db_found) {           
-        $sql = "INSERT INTO posts (post_id, username, privacy, type, text, content, timestamp, id_shared_post) VALUES ('','".$_SESSION["myusername"]."', 'contacts', null, 'sharing', null, NOW(), '".$_POST["post_id"]."')";
-echo $sql;
+        $sql = "INSERT INTO posts (post_id, username, privacy, type, text, content, timestamp, id_shared_post) VALUES ('','".$_SESSION["myusername"]."', 'contacts', null, 'shared a post', null, NOW(), '".$_POST["post_id"]."')";
+
         $result = mysqli_query($db_handle, $sql) or die(mysql_error());
-        /*
-        $sql = "INSERT INTO notifications (notif_id, parent_id, type, seen, timestamp, user_create, user_receive) VALUES ('', '".$_POST["post_id"]."', 'comment','0', NOW(), '".$_SESSION["myusername"]."', (SELECT username FROM posts WHERE post_id = ".$_POST["post_id"]."))";
         
-        $result = mysqli_query($db_handle, $sql) or die(mysql_error());*/
+        $sql = "INSERT INTO notifications (notif_id, parent_id, type, seen, timestamp, user_create, user_receive) VALUES ('', '".$_POST["post_id"]."', 'share','0', NOW(), '".$_SESSION["myusername"]."', (SELECT username FROM posts WHERE post_id = ".$_POST["post_id"]."))";
+        
+        $result = mysqli_query($db_handle, $sql) or die(mysql_error());
     }
     else { echo "Base de données non trouvée."; }
 
     mysqli_close($db_handle);
 
-    //header('Location:home.php');
+    header('Location:home.php');
 ?>
